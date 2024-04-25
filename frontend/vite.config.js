0ffba805f935fd84aedc0cbd2619c0438aa8e2d6
jvk <jvkkoppineedi@gmail.com>
2024-04-25 22:40:10 +0530

    add env variable backend url to frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  server:{
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '') // Load .env file
+  return {
+    server:{
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URI,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
-    }
-  },
-  plugins: [react()],
+    },
+    plugins: [react()],
+  }
 })
